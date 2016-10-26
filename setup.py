18c66755921ddd5ad253187d8664028c8ebe3265
Daniel Neuhäuser <ich@danielneuhaeuser.de>
2016-10-26 13:01:26 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+"""
+    setup
+    ~~~~~
+
+    :copyright: 2016 by Daniel Neuhäuser
+    :license: BSD, see LICENSE.rst for details
+"""
+
+from setuptools import setup
+
+
+setup(
+    name='feedopml',
+    version='0.1',
+    author='Daniel Neuhäuser',
+    author_email='ich@danielneuhaeuser.de',
+    install_requires=['lxml'],
+    py_modules=['feedopml']
+)
