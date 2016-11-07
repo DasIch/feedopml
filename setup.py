4abb799cca439c9cd71e0f7a3d07b034fefc6408
Daniel Neuhäuser <ich@danielneuhaeuser.de>
2016-11-07 14:04:58 +0100

    add url to package metadata

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ from setuptools import setup
 setup(
     name='feedopml',
     version='0.1',
+    url='https://github.com/DasIch/feedopml',
     author='Daniel Neuhäuser',
     author_email='ich@danielneuhaeuser.de',
     install_requires=['lxml'],
